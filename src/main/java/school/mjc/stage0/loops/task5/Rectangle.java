fa79fc7021a9a09620b7939f1a8679d796f7f108
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-12-18 13:11:47 +0600

    rf

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,26 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
+        PrintHorizontal(length);
+        for(int i = 0; i < height-2; i++){
+            PrintVertical(length);
+        }
+        PrintHorizontal(length);
+    }
 
+
+    public static void PrintHorizontal(int length){
+        for(int i = 0; i < length-1; i++){
+            System.out.print(8);
+        }
+        System.out.println(8);
+    }
+
+    public static void PrintVertical(int length){
+        System.out.print(8);
+        for(int i = 0; i < length-2; i++){
+            System.out.print(" ");
+        }
+        System.out.println(8);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
+        PrintHorizontal(sideLength);
+        for(int i = 0; i < sideLength-2; i++){
+            PrintVertical(sideLength);
+        }
+        PrintHorizontal(sideLength);
+    }
 
+    public static void PrintHorizontal(int sideLength){
+        for(int i = 0; i < sideLength-1; i++){
+            System.out.print(8);
+        }
+        System.out.println(8);
+    }
+
+    public static void PrintVertical(int sideLength){
+        System.out.print(8);
+        for(int i = 0; i < sideLength-2; i++){
+            System.out.print(" ");
+        }
+        System.out.println(8);
     }
 }
